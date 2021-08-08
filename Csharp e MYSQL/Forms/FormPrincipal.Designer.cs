561bef2f8d17d3ca84e962bdabdc537652afde75
petersonstadler2 <petersonstadler@gmail.com>
2021-08-08 09:31:35 -0300

    Modelado o FormPrincipal

--- Csharp e MYSQL/Forms/FormPrincipal.Designer.cs
+++ Csharp e MYSQL/Forms/FormPrincipal.Designer.cs
@@ -29,20 +29,100 @@ namespace Csharp_e_MYSQL
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridMain = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.txtBoxPesquisa = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMain)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridMain
+            // 
+            this.dataGridMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridMain.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridMain.Location = new System.Drawing.Point(90, 44);
+            this.dataGridMain.Name = "dataGridMain";
+            this.dataGridMain.Size = new System.Drawing.Size(567, 423);
+            this.dataGridMain.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(72, 59);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Novo";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(12, 77);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(72, 59);
+            this.btnAlterar.TabIndex = 2;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(12, 142);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(72, 59);
+            this.btnRemover.TabIndex = 3;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPesquisar.Location = new System.Drawing.Point(579, 12);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(78, 26);
+            this.btnPesquisar.TabIndex = 4;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxPesquisa
+            // 
+            this.txtBoxPesquisa.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBoxPesquisa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtBoxPesquisa.Location = new System.Drawing.Point(90, 12);
+            this.txtBoxPesquisa.Name = "txtBoxPesquisa";
+            this.txtBoxPesquisa.Size = new System.Drawing.Size(483, 26);
+            this.txtBoxPesquisa.TabIndex = 5;
             // 
             // FormPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(679, 439);
+            this.ClientSize = new System.Drawing.Size(669, 479);
+            this.Controls.Add(this.txtBoxPesquisa);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridMain);
             this.Name = "FormPrincipal";
             this.Text = "Principal";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridMain)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridMain;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.TextBox txtBoxPesquisa;
     }
 }
 
