5370425150d601d7fc49c8fc7de0c09f6f4e9883
petersonstadler2 <petersonstadler@gmail.com>
2021-08-08 08:59:27 -0300

    Inicio do projeto

---
+++ Csharp e MYSQL/Classes/DatabaseConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Csharp_e_MYSQL.Classes
+{
+    class DatabaseConnection
+    {
+        private string dbhost, dbnome, dbuser, dbpass;
+        private int dbport;
+        private MySqlConnection connection;
+        private MySqlCommand command;
+
+        public string Dbhost { get => dbhost; set => dbhost = value; }
+        public string Dbnome { get => dbnome; set => dbnome = value; }
+        public string Dbuser { get => dbuser; set => dbuser = value; }
+        public string Dbpass { get => dbpass; set => dbpass = value; }
+        public int Dbport { get => dbport; set => dbport = value; }
+
+        public void Connect()  //ABRE CONEXÃO COM O BANCO DE DADOS
+        {
+            string ConString = $"server={dbhost};database={dbnome};uid={dbuser};pwd={dbpass};port={dbport}";
+            try
+            {
+                connection = new MySqlConnection(ConString);
+                command = connection.CreateCommand();
+                connection.Open();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Falha ao conectar-se com o banco de dados! \n\n" + e, "Abrir Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void Close()
+        {
+            try
+            {
+                connection.Close();
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Falha ao fechar conexão com o banco de dados! \n\n" + e, "Fechar Conexão", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+    }
+}
---
+++ Csharp e MYSQL/Forms/FormPrincipal.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Csharp_e_MYSQL
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(679, 439);
+            this.Name = "FormPrincipal";
+            this.Text = "Principal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
