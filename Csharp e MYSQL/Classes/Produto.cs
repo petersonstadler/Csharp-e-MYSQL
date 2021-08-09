d6ad410b54291e0bb7cf5646c442c4b9ff50899e
petersonstadler2 <petersonstadler@gmail.com>
2021-08-09 08:05:24 -0300

    Criado a Classe Produto

---
+++ Csharp e MYSQL/Classes/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_e_MYSQL.Classes
+{
+    class Produto
+    {
+        private int id, cod;
+        private string nome;
+        decimal custo, venda;
+
+        public int Id { get => id; set => id = value; }
+        public int Cod { get => cod; set => cod = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public decimal Custo { get => custo; set => custo = value; }
+        public decimal Venda { get => venda; set => venda = value; }
+    }
+}
